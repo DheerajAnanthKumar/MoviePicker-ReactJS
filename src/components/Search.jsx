4e7300e8c780aa2697d4da5a3032da31cf899df9
DheerajAnanthKumar <158621436+DheerajAnanthKumar@users.noreply.github.com>
2025-06-06 14:28:53 -0400

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  return (
+    <div className="search">
+      <div>
+        <img src="../../public/search.svg" />
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
